35f10e5ab9438f39553e9630120ef00a8fdbc1de
keeperazra <alexander.zimmerman@mymail.champlain.edu>
2021-03-16 14:48:34 -0400

    config files at end of project 4

---
+++ config/web01/config.php
@@ -0,0 +1,23 @@
+<?php
+// config.php written at Tue Mar 16 15:40:08 2021
+// detailed description https://wackowiki.org/doc/Doc/English/Configuration
+// do not change wacko_version manually!
+
+$wacko_config = [
+	'base_url' => 'http://10.0.5.100/',
+	'database_charset' => 'utf8mb4',
+	'database_collation' => 'utf8mb4_unicode_520_ci',
+	'database_driver' => 'mysql_pdo',
+	'database_engine' => 'InnoDB',
+	'database_host' => '10.0.5.10',
+	'database_port' => '3306',
+	'database_database' => 'wacko',
+	'database_user' => 'web',
+	'database_password' => 'unsecurepassword',
+	'sql_mode_strict' => '0',
+	'table_prefix' => 'wacko_',
+	'system_seed' => '*S[Qh9827yin5f2GGlnB',
+	'recovery_password' => '$2y$10$Cx5FZbljMlVWNLHkGnrDk.t.UF6PistgeMkdbZcEEePIXiD21CVu.',
+	'hashid_seed' => 'fy&1VQn98638#BC5{I5x',
+	'wacko_version' => '6.0.18',
+];
